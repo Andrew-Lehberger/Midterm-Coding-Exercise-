67a9a180911956e803a0cec3ee860f60b2c538b0
alehb <alehb@128.4.74.250>
2017-10-17 16:35:58 -0400

    tests

--- Midterm Coding Project/src/test/java/com/cisc181/core/Staff_Test.java
+++ Midterm Coding Project/src/test/java/com/cisc181/core/Staff_Test.java
@@ -16,17 +16,38 @@ import antlr.collections.List;
 
 
 public class Staff_Test {
-	ArrayList StaffMembers = new ArrayList();
-	Staff_Test.add("Mike", "J", "Smith", 07/12/1985, "1 Main St", "3024558974", "msmith@udel.edu", "Tues 3pm", 4, 95000, 02/14/2016), MR);
-
-
-	@BeforeClass
-	public static void setup() {
-	}
 	
 	@Test
-	public void test() {
-		assertEquals(1,1);
-	}	
+	public void test1() {
+		
+	
+	ArrayList<Staff> StaffMembers = new ArrayList<Staff>();
+	
+	StaffMembers.add(new Staff("Mike", "J", "Smith", Person.getDOB(), "1 Main St", "3024558974", "msmith@udel.edu", "Tues 3pm", 4, 95000.00, null, eTitle.MR));
+	StaffMembers.add(new Staff("Mary", "E", "Jones", Person.getDOB(), "4 Oak St", "3024758074", "mjones@udel.edu", "Tues 3pm", 4, 105000.00, null, eTitle.MRS));
+	StaffMembers.add(new Staff("Karen", "M", "Miller", Person.getDOB(), "20 Main St", "3024058111", "kmiller@udel.edu", "Thurs 3pm", 4, 75000.00, null, eTitle.MS));
+	StaffMembers.add(new Staff("Tom", "A", "Rodgers", Person.getDOB(), "570 Maple Rd", "3028762309", "trodgers@udel.edu", "Thurs 3pm", 4, 175000.00, null, eTitle.MR));
+	StaffMembers.add(new Staff("Jose", "R", "Lopez", Person.getDOB(), "17 Main St", "3025055174", "jlopez@udel.edu", "Tues 1pm", 4, 100000.00, null, eTitle.MR));
+
+	assertEquals(110,000, (StaffMembers.getSalary()/5));
+	
+	try {
+			Staff.DOB.isEmpty();
+			Staff.hireDate.isEmpty();}
+		
+		catch(PersonException p) {
+			
+		}
+		
+		
+		
+	}
+	
+	}
+	
+	
+			
+
+
 
 }
--- Midterm Coding Project/src/test/java/com/cisc181/core/Student_Test.java
+++ Midterm Coding Project/src/test/java/com/cisc181/core/Student_Test.java
@@ -8,14 +8,42 @@ import java.util.Calendar;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import com.cisc181.eNums.eMajor;
+
 public class Student_Test {
 
 	@BeforeClass
 	public static void setup() {
+		ArrayList<Course> CourseList = new ArrayList<Course>();
+		
+		CourseList.add(new Course("John", "A", "Thompson", null, eMajor.COMPSI, "12 RiverSide DR", "3024449597", "jthompson@udel.edu", "CISC 181", 80));
+		CourseList.add(new Course("Ryan", "J", "Muller", null, eMajor.COMPSI, "30 RiverSide DR", "3025449597", "rmuller@udel.edu", "CISC 181", 70));
+		CourseList.add(new Course("Mary", "A", "Smith", null, eMajor.COMPSI, "105 RiverSide DR", "3026449597", "msmith@udel.edu", "CISC 181", 90));
+	
+		ArrayList<Semester> SemesterList = new ArrayList<Semester>();
+		
+		SemesterList.add(new Semester("John", "A", "Thompson", null, eMajor.COMPSI, "12 RiverSide DR", "3024449597", "jthompson@udel.edu", "CISC 181", 80, null, null));
+		SemesterList.add(new Semester ("Ryan", "J", "Muller", null, eMajor.COMPSI, "30 RiverSide DR", "3025449597", "rmuller@udel.edu", "CISC 181", 70, null, null));
+		
+		ArrayList<Student> StudentList = new ArrayList<Student>();
+		
+		StudentList.add(new Student("John", "A", "Thompson", null, eMajor.COMPSI, "12 RiverSide DR", "3024449597", "jthompson@udel.edu"));
+		StudentList.add(new Student("James", "A", "Johnson", null, eMajor.COMPSI, "12 RiverSide DR", "3024449597", "jthompson@udel.edu"));
+		StudentList.add(new Student("Mike", "A", "Cuban", null, eMajor.COMPSI, "12 RiverSide DR", "3024449597", "jthompson@udel.edu"));
+		StudentList.add(new Student("Ann", "A", "Jones", null, eMajor.COMPSI, "12 RiverSide DR", "3024449597", "jthompson@udel.edu"));
+		StudentList.add(new Student("Kate", "A", "Smith", null, eMajor.COMPSI, "12 RiverSide DR", "3024449597", "jthompson@udel.edu"));
+		StudentList.add(new Student("Lauren", "A", "Doe", null, eMajor.COMPSI, "12 RiverSide DR", "3024449597", "jthompson@udel.edu"));
+		StudentList.add(new Student("Andrew", "A", "Goldberg", null, eMajor.COMPSI, "12 RiverSide DR", "3024449597", "jthompson@udel.edu"));
+		StudentList.add(new Student("AJ", "A", "Smith", null, eMajor.COMPSI, "12 RiverSide DR", "3024449597", "jthompson@udel.edu"));
+		StudentList.add(new Student("Maria", "A", "Johnson", null, eMajor.COMPSI, "12 RiverSide DR", "3024449597", "jthompson@udel.edu"));
+		StudentList.add(new Student("Steve", "A", "Li", null, eMajor.COMPSI, "12 RiverSide DR", "3024449597", "jthompson@udel.edu"));
+		StudentList.add(new Student("Michelle", "A", "Thompson", null, eMajor.COMPSI, "12 RiverSide DR", "3024449597", "jthompson@udel.edu"));
+		
+	
 	}
 
 	@Test
 	public void test() {
-		assertEquals(1, 1);
+		assertEquals(80, (CourseList.getGradePoints()/3));
 	}
 }
